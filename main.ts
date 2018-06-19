0afa00018640e03bf5bb5b935edf5eb231e17157
Donovan Bergin <kaiserbergin@gmail.com>
2018-06-19 15:35:47 -0500

    RxJs xmlHttpRequest with subscriber rendering DOM
    This implementation will have the observer subscribe to the returned observer and call a function to render the result in the DOM

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-import { fromEvent } from "rxjs";
-import { map, filter, onErrorResumeNext, delay } from "rxjs/operators";
+import { fromEvent, Observable } from "rxjs";
+import { map, filter, onErrorResumeNext, delay, flatMap } from "rxjs/operators";
 
 let output = document.getElementById("output");
 let button = document.getElementById("button");
@@ -7,23 +7,35 @@ let button = document.getElementById("button");
 let click = fromEvent(button, "click");
 
 function load(url: string) {
-    let xhr = new XMLHttpRequest();
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
 
-    xhr.addEventListener("load", () => {
-        let movies = JSON.parse(xhr.responseText);
-        movies.forEach(movie => {
-            let div = document.createElement("div");
-            div.innerText = movie.title;
-            output.appendChild(div);
-        });
+        xhr.addEventListener("load", () => {
+            let data = JSON.parse(xhr.responseText);
+            observer.next(data);
+            observer.complete;
+        })
+
+        xhr.open("GET", url);
+        xhr.send();
     })
-
-    xhr.open("GET", url);
-    xhr.send();
 }
 
-click.subscribe(
-    e => load("movies.json"),
+function renderMovies(movies) {
+    movies.forEach(movie => {
+        let div = document.createElement("div");
+        div.innerText = movie.title;
+        output.appendChild(div);
+    });
+}
+
+//Although load is called, it is not returned to anyone until someone subscribes.
+load("movies.json");
+
+click.pipe(
+    flatMap(e => load("movies.json"))
+).subscribe(
+    renderMovies,
     e => console.log(`error: ${e}`),
     () => console.log("complete")
 );
