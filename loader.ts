ed814ded05888aa5c1d8ce23d3025420ff0597b1
Donovan Bergin <kaiserbergin@gmail.com>
2018-06-19 22:12:41 -0500

    xmlHttpRequest obs with abort
    Ensured that examples show how unsub and errors will call the unsub method in the observable which in turn removes the listener and aborts the xhr request

--- loader.ts
+++ loader.ts
@@ -1,11 +1,12 @@
-import { Observable, defer, from } from "rxjs";
-import { retryWhen, scan, takeWhile, delay, flatMap } from "rxjs/operators"
+import { Observable, defer, from, of } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { retryWhen, scan, map, delay, flatMap, switchMap } from "rxjs/operators"
 
 export function load(url: string) {
     return Observable.create(observer => {
         let xhr = new XMLHttpRequest();
 
-        xhr.addEventListener("load", () => {
+        let onLoad = () => {
             if (xhr.status === 200) {
                 let data = JSON.parse(xhr.responseText);
                 observer.next(data);
@@ -13,10 +14,18 @@ export function load(url: string) {
             } else {
                 observer.error(xhr.statusText);
             }
-        })
+        }
+
+        xhr.addEventListener("load", onLoad)
 
         xhr.open("GET", url);
         xhr.send();
+
+        return () => {
+            console.log("cleanup");
+            xhr.removeEventListener("load", onLoad);
+            xhr.abort();
+        }
     }).pipe(
         retryWhen(retryStrategy({ attempts: 3, delayMs: 1500 }))
     );
@@ -38,6 +47,25 @@ export function loadWithFetch(url: string) {
     );
 }
 
+//fix this thing
+function retryXHRStrategy(xhr, { attempts = 4, delayMs = 200 } = {}) {
+    return function (errors) {
+        return errors.pipe(
+            scan((acc, value) => {
+                acc += 1;
+                if (acc < attempts) {
+                    return acc;
+                } else {
+                    xhr.abort();
+                    throw new Error(`error thrown in retryStrategy with value: ${value.toString()}`);
+                }
+            }, 0),
+            delay(delayMs)
+        );
+    }
+}
+
+
 function retryStrategy({ attempts = 4, delayMs = 200 } = {}) {
     return function (errors) {
         return errors.pipe(
@@ -46,7 +74,7 @@ function retryStrategy({ attempts = 4, delayMs = 200 } = {}) {
                 if (acc < attempts) {
                     return acc;
                 } else {
-                    throw new Error(value.toString());
+                    throw new Error(`error thrown in retryStrategy with error message: ${value.toString()}`);
                 }
             }, 0),
             delay(delayMs)
--- main.ts
+++ main.ts
@@ -7,8 +7,6 @@ let button = document.getElementById("button");
 
 let click = fromEvent(button, "click");
 
-
-
 function renderMovies(movies) {
     movies.forEach(movie => {
         let div = document.createElement("div");
@@ -19,17 +17,32 @@ function renderMovies(movies) {
 
 //Although load is called, it is not returned to anyone until someone subscribes.
 //load("movies.json").subscribe(renderMovies);
-loadWithFetch("moviess.json")
+let subscription = load("testCancel.json")
     .subscribe(
         renderMovies,
         e => console.log(`error: ${e}`),
         () => console.log("complete")
     );
 
-// click.pipe(
-//     flatMap(e => loadWithFetch("movies.json"))
-// ).subscribe(
-//     renderMovies,
-//     e => console.log(`error: ${e}`),
-//     () => console.log("complete")
-// );
+console.log(subscription);
+
+subscription.unsubscribe();
+
+load("movies.json").subscribe(
+    renderMovies,
+    e => console.log(`ui error: ${e}`),
+    () => console.log("complete")
+);
+load("moviess.json").subscribe(
+    e => console.log(e),
+    e => console.log(`ui error: ${e}`),
+    () => console.log("complete")
+);
+
+click.pipe(
+    flatMap(e => load("games.json"))
+).subscribe(
+    renderMovies,
+    e => console.log(`error: ${e}`),
+    () => console.log("complete")
+);
